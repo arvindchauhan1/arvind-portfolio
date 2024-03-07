bef503be8a31b86a6c70aa6d6e5333358cf8daec
Arvind Chauhan <arvindchn1@gmail.com>
2024-03-07 20:44:24 +0530

    update

--- Prayagraj/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Script")
-
-const menuToggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.showcase')
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    showcase.classList.toggle('active');
-})
